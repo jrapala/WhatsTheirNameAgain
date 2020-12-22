6ef39119fa9f03c60d2cb2ea96dd87d74b22c3b6
Juliette Rapala <jrapala@gmail.com>
2020-12-22 16:46:07 -0600

    Initial Commit

---
+++ WhatsTheirNameAgain/WhatsTheirNameAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTheirNameAgainApp.swift
+//  WhatsTheirNameAgain
+//
+//  Created by Juliette Rapala on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTheirNameAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
